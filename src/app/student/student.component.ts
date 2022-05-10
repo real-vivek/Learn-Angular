fb1f438c0ceb632b794a0e192a1f9557251c04f4
Vivek Yadav <vivek.mahadev.yadav@gmail.com>
2022-05-10 10:56:49 +0530

    Example of data binding using String Interpolation

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -2,21 +2,13 @@ import { Component } from "@angular/core";
 
 //To create new component without using cli create a folder with component name and create html,css and ts file
 @Component({
-    selector:'.app-student',
-    template:'<h2>Student Component Works!<h2><br><h3>Hello there</h3>',
-    styles:[`
-        h2{
-            color: red
-        }
-    `,
-        `
-        h3{
-            color: blue
-        }
-        `
-]
+    selector:'app-student',
+    templateUrl:'./student.component.html',
+    styleUrls:['./student.component.css']
 })
 //We export this class because we need to register it in app module
 export class StudentComponent{
-
+    //number can be converted to string so it can be used for string interpolcation
+    studentId=1;
+    studentStatus='present';
 }
